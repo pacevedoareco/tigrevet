59997e1a5adab418f693d0b535f5e8c2f14351f6
Pablo Acevedo Areco (estudiante) <pacevedoareco@uade.edu.ar>
2024-10-09 23:26:31 +0200

    cerrar el menu al hacer click en main

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 	$( "#calendario" ).datepicker();
+
+	$("main").click(()=>{
+		if($("#menuHamburguesa input").prop("checked"))
+			$("#menuHamburguesa input").click();
+	});
 });
